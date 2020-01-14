2daebad1d1c12788d79bc0ade027cb23a8daac8b
knightyun <2386209384@qq.com>
2020-01-14 12:18:15 +0800

    添加页面右下方功能按钮：目录、搜索、顶部、底部

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,33 @@
-(function($){
-  $(function(){
-    $(".button-collapse").sideNav();
-  });
-})(jQuery);
+M.AutoInit();
+
+// (function($){
+//   $(function(){
+//     $(".sidenav").sidenav();
+//   });
+// })(jQuery);
+
+// $(document).ready(function(){
+//   $('.sidenav').sidenav();
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elemCategory = document.querySelector('#category');
+    M.Sidenav.init(elemCategory, {
+        'draggable': true,
+        'edge': 'right'
+    });
+
+    // var elemsCarousel = document.querySelectorAll('.carousel');
+    // M.Carousel.init(elemsCarousel, {
+    //     'dist': 0,
+    //     'numVisible': 1,
+    //     'duration': 500,
+    //     'indicators': true,
+    //     'fullWidth': false
+    // });
+
+    // var instCarousel = M.Carousel.getInstance(document.querySelector('.carousel'));
+    // setInterval(function() {
+    //     instCarousel.next();
+    // }, 2000);
+});
