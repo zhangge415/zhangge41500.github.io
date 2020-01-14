bed4ac97d0972be5f82731e28595eb1d7ce59cdb
knightyun <2386209384@qq.com>
2020-01-14 21:01:24 +0800

    添加顶部粒子连线效果

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ M.AutoInit();
 document.addEventListener('DOMContentLoaded', function () {
     var elemCategory = document.querySelector('#category');
     M.Sidenav.init(elemCategory, {
-        'draggable': true,
         'edge': 'right'
     });
 
@@ -21,3 +20,46 @@ document.addEventListener('DOMContentLoaded', function () {
     //     instCarousel.next();
     // }, 2000);
 });
+
+Particles.init({
+    /*
+    Option           | Type               | Default | Description
+    =====================================================================================================
+    selector         | string             | -       | Required: CSS selector of your canvas element
+    maxParticles     | integer            | 100     | Optional: Maximum amount of particles
+    sizeVariations   | integer            | 3       | Optional: Amount of size variations
+    speed            | integer            | 0.5     | Optional: Movement speed of the particles
+    color            | string or string[] | #000000 | Optional: Color(s) of the particles and connecting lines
+    minDistance      | integer            | 120     | Optional: Distance in px for connecting lines
+    connectParticles | boolean            | false   | Optional: true/false if connecting lines should be drawn
+    responsive       | array              | null    | Optional: Array of objects containing breakpoints and options
+     */
+    selector: '.particles',
+    maxParticles: 100,
+    sizeVariations: 5,
+    speed: 0.5,
+    color: ['#ffffff', '#eeeeee', '#dddddd', '#cccccc', '#bbbbbb', '#aaaaaa'],
+    minDistance: 100,
+    connectParticles: true,
+    responsive: [{
+        breakpoint: 768,
+        options: {
+            maxParticles: 60
+        }
+    },{
+        breakpoint: 425,
+        options: {
+            maxParticles: 50
+        }
+    },{
+        breakpoint: 375,
+        options: {
+            maxParticles: 40
+        }
+    },{
+        breakpoint: 320,
+        options: {
+            maxParticles: 30
+        }
+    }]
+})
